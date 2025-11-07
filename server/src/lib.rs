364765f3d56af45376b46df7db0158481645e5b0
Frawley <frawley@duck.com>
2025-11-06 19:14:30 -0500

    Set up route in edge function

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,20 +1,36 @@
-use axum::{Router, routing::get};
+use std::{fmt, sync::Arc};
+
+use axum::{Router, extract::State, http::StatusCode, routing::post};
 use tower_service::Service;
 use worker::*;
 
-fn router() -> Router {
-    Router::new().route("/", get(root))
+struct AppState {
+    pub db: D1Database,
+}
+
+impl fmt::Debug for AppState {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("AppState").finish_non_exhaustive()
+    }
+}
+
+fn router(state: AppState) -> Router {
+    Router::new()
+        .route("/backups", post(post_backup))
+        .with_state(Arc::new(state))
 }
 
 #[event(fetch)]
 async fn fetch(
     req: HttpRequest,
-    _env: Env,
+    env: Env,
     _ctx: Context,
 ) -> Result<axum::http::Response<axum::body::Body>> {
-    Ok(router().call(req).await?)
+    let state = AppState { db: env.d1("DB")? };
+    Ok(router(state).call(req).await?)
 }
 
-pub async fn root() -> &'static str {
-    "Hello Axum!"
+#[axum::debug_handler]
+pub async fn post_backup(State(state): State<Arc<AppState>>) -> StatusCode {
+    todo!()
 }
